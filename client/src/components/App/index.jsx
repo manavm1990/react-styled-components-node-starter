3e2aaad0d2377d41844dfd282e9911aca9f8bf8a
Manav Misra <manavm@visionwebsoft.com>
2020-10-28 22:00:20 -0500

    :building_construction: Move 'App' into 'components

---
+++ client/src/components/App/index.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+import styled from "styled-components";
+
+import logo from "./logo.svg";
+
+const AppContainer = styled.div`
+  text-align: center;
+
+  .logo {
+    height: 40vmin;
+    pointer-events: none;
+  }
+
+  @media (prefers-reduced-motion: no-preference) {
+    .logo {
+      animation: App-logo-spin infinite 20s linear;
+    }
+  }
+
+  header {
+    align-items: center;
+    background-color: #282c34;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    font-size: calc(10px + 2vmin);
+    justify-content: center;
+    min-height: 100vh;
+  }
+
+  .link {
+    color: #61dafb;
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+export default () => (
+  <AppContainer>
+    <header>
+      <img src={logo} className="logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </AppContainer>
+);
